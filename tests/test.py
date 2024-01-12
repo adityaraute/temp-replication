5640a9c92127ed61feae3da596a1b1b935f64cb9
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2024-01-12 13:33:56 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,30 +1,20 @@
 import pytest
 
-
-def test_div_zero_exception():
-    """
-    pytest.raises can assert that exceptions are raised (catching them)
-    """
-    with pytest.raises(ZeroDivisionError):
-        x = 1 / 0
+from myapp.app import multiply_by_two, divide_by_two
 
 
-def test_keyerror_details():
-    """
-    The raised exception can be referenced, and further inspected (or asserted)
-    """
-    my_map = {"foo": "bar"}
-
-    with pytest.raises(KeyError) as ke:
-        baz = my_map["baz"]
-
-    # Our KeyError should reference the missing key, "baz"
-    assert "baz" in str(ke)
+@pytest.fixture
+def numbers():
+    a = 10
+    b = 20
+    return [a,b]
 
 
-def test_approximate_matches():
-    """
-    pytest.approx can be used to assert "approximate" numerical equality
-    (compare to "assertAlmostEqual" in unittest.TestCase)
-    """
-    assert 0.1 + 0.2 == pytest.approx(0.3)
+class TestApp:
+    def test_multiplication(self, numbers):
+        res = multiply_by_two(numbers[0])
+        assert res == numbers[1]
+
+    def test_division(self, numbers):
+        res = divide_by_two(numbers[1])
+        assert res == numbers[0]
