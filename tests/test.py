7d8ca17d44c85c0a0d563884faeaf98cc2033d45
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2024-01-12 13:40:22 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,10 @@
-import pytest
+def add_two_things(a, b):
+    """
+    ```py
+    from my_lib import add_two_things
 
-from myapp.app import multiply_by_two, divide_by_two
-
-
-@pytest.fixture
-def numbers():
-    a = 10
-    b = 20
-    return [a,b]
-
-
-class TestApp:
-    def test_multiplication(self, numbers):
-        res = multiply_by_two(numbers[0])
-        assert res == numbers[1]
-
-    def test_division(self, numbers):
-        res = divide_by_two(numbers[1])
-        assert res == numbers[0]
+    print(add_two_things(1, 2))
+    #> 3
+    ```
+    """
+    return a + b
