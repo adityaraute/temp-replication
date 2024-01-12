bfabc85b30b84aacd386252115ba284199207477
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2024-01-12 13:49:46 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,3 @@
-def test_add_numbers():
-    # Test case 1
-    result = add_numbers(3, 5)
-    assert result == 8
-
-    # Test case 2
-    result = add_numbers(-2, 7)
-    assert result == 5
-
-    # Test case 3
-    result = add_numbers(-4, -6)
-    assert result == -10
+def test_calculation():
+    assert 2+2 == 4
+    
