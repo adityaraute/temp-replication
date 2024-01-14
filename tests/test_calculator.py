df6d0de5376d8a123167d0fbb99b12c2993c70e7
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2024-01-14 08:40:32 +0530

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -12,9 +12,9 @@ def subtract(x, y):
 
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
     assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(0, 0) == 1
 
 def test_subtract():
     assert subtract(5, 2) == 3
